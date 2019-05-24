437c4317ba40c6c61059526a0ef26c3550858426
Михаил Комаров <mikhailkomarov@MacBook-Air-Mihail.local>
2019-05-24 03:01:13 +0300

    Revert "first commit"
    This reverts commit 63c7f2e0f234e5c667302aee392359a3dc49324e.
    
    e Please enter the commit message for your changes. Lines starting

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-TOKEN = '588048354:AAFgl8K6V4JSoc9Cs3v4PlgNT2zfNHg0XcU'
-APP_NAME = 'voyage-livre-89482.herokuapp.com'
-HOST = "0.0.0.0"
-PORT = int(os.environ.get('PORT', 5000))
-DATABASE_URL = os.environ['DATABASE_URL']
