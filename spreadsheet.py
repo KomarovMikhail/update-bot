c1c0b99aebde0ed5c72047c7754730872c4c788c
Михаил Комаров <mikhailkomarov@MacBook-Air-Mihail.local>
2019-05-29 00:18:12 +0300

    Some fixes

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,15 +2,17 @@ from config import *
 from googleapiclient.discovery import build
 import httplib2
 from oauth2client.service_account import ServiceAccountCredentials
+from spreadsheet_handler import build_link
 
 
 def get_spreadsheet():
+    spreadsheet_id = build_link()
     credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
                                                                    ['https://www.googleapis.com/auth/spreadsheets'])
     http_auth = credentials.authorize(httplib2.Http())
     service = build('sheets', 'v4', http=http_auth)
     result = service.spreadsheets().values().get(
-        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        spreadsheetId=spreadsheet_id, range=RANGE_NAME).execute()
     spreadsheet = result.get('values')
     result = (
         int(spreadsheet[0][1]),
